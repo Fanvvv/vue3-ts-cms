817207530cb4ceafe21f4f85c2ba46c4747b1344
fan <291950304@qq.com>
2022-01-22 10:39:46 +0800

    feat: 添加基础样式

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/index'
 import store from '@/store/index'
+import 'normalize.css'
+import './assets/css/index.scss'
 
 const app = createApp(App)
 
